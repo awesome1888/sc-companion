3a0ff2eaead2be4931b2449d680eed2001480e46
Sergei <gannochenko.sv@gmail.com>
2019-03-08 16:42:06 +0100

    first example

--- demo/src/pages/index.js
+++ demo/src/pages/index.js
@@ -5,10 +5,7 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-import * as S from "../lib/"
-// import * as S from 'sc-companion';
-//
-console.dir(S)
+import * as S from "../components/aux-style.js"
 
 const IndexPage = () => (
   <Layout>
@@ -23,8 +20,91 @@ const IndexPage = () => (
       front-end.
     </p>
 
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <h2>align()</h2>
+    <p>Uses flexbox to produce aligned containers.</p>
+
+    <div>
+      <S.C>
+        <S.RowTopLeft>
+          <S.BlueSquare />
+          <S.BlueSquare />
+        </S.RowTopLeft>
+        <S.RowTopCenter>
+          <S.BlueSquare />
+          <S.BlueSquare />
+        </S.RowTopCenter>
+        <S.RowTopRight>
+          <S.BlueSquare />
+          <S.BlueSquare />
+        </S.RowTopRight>
+
+        <S.RowCenterLeft>
+          <S.BlueSquare />
+          <S.BlueSquare />
+        </S.RowCenterLeft>
+        <S.RowCenterCenter>
+          <S.BlueSquare />
+          <S.BlueSquare />
+        </S.RowCenterCenter>
+        <S.RowCenterRight>
+          <S.BlueSquare />
+          <S.BlueSquare />
+        </S.RowCenterRight>
+
+        <S.RowBottomLeft>
+          <S.BlueSquare />
+          <S.BlueSquare />
+        </S.RowBottomLeft>
+        <S.RowBottomCenter>
+          <S.BlueSquare />
+          <S.BlueSquare />
+        </S.RowBottomCenter>
+        <S.RowBottomRight>
+          <S.BlueSquare />
+          <S.BlueSquare />
+        </S.RowBottomRight>
+      </S.C>
+
+      <S.C>
+        <S.ColumnTopLeft>
+          <S.BlueSquare />
+          <S.BlueSquare />
+        </S.ColumnTopLeft>
+        <S.ColumnTopCenter>
+          <S.BlueSquare />
+          <S.BlueSquare />
+        </S.ColumnTopCenter>
+        <S.ColumnTopRight>
+          <S.BlueSquare />
+          <S.BlueSquare />
+        </S.ColumnTopRight>
+
+        <S.ColumnCenterLeft>
+          <S.BlueSquare />
+          <S.BlueSquare />
+        </S.ColumnCenterLeft>
+        <S.ColumnCenterCenter>
+          <S.BlueSquare />
+          <S.BlueSquare />
+        </S.ColumnCenterCenter>
+        <S.ColumnCenterRight>
+          <S.BlueSquare />
+          <S.BlueSquare />
+        </S.ColumnCenterRight>
+
+        <S.ColumnBottomLeft>
+          <S.BlueSquare />
+          <S.BlueSquare />
+        </S.ColumnBottomLeft>
+        <S.ColumnBottomCenter>
+          <S.BlueSquare />
+          <S.BlueSquare />
+        </S.ColumnBottomCenter>
+        <S.ColumnBottomRight>
+          <S.BlueSquare />
+          <S.BlueSquare />
+        </S.ColumnBottomRight>
+      </S.C>
     </div>
 
     <Link to="/page-2/">Go to page 2</Link>
